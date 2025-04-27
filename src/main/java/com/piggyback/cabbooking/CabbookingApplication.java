e4822aabbd646e23752c1098744c14709d3bd35b
Wolf1824 <makhijaniankush24@gmail.com>
2025-04-27 14:37:31 -0400

    first commit

---
+++ src/main/java/com/piggyback/cabbooking/CabbookingApplication.java
@@ -0,0 +1,13 @@
+package com.piggyback.cabbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabbookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piggyback/cabbooking/CabbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piggyback.cabbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
